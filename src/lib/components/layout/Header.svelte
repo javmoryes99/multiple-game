b56e5ea5deaa6245adda2d9d48549b0aff57d6d5
javmoryes99 <jmoryes99@gmail.com>
2026-09-03 12:47:07 +0200

    style: header and typography

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -7,12 +7,14 @@
   <header class="sticky top-0 bg-neutral">
   <div class="navbar shadow-sm">
     <div class="navbar-start">
-      <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="" class="h-8 w-auto" />
-      <a class="btn btn-ghost text-xl text-neutral-content">Diviguessr</a>
+      <button class="btn btn-ghost text-xl text-neutral-content border-none hover:bg-transparent hover:opacity-100">
+        <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="" class="h-8 w-auto" />
+        <span>Diviguessr</span>
+      </button>
     </div>
     <div class="navbar-center hidden lg:flex">
     </div>
-    <div class="navbar-end">
+    <div class="navbar-end pr-5">
       <ThemeController />
     </div>
   </div>
